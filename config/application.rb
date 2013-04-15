4be6f3f2b060c9474726321d2862bacde18c6d73
FND <fnd@localhost.localdomain>
2013-04-15 08:03:09 +0200

    tweaked configuration for Heroku staging environment
    cf. https://devcenter.heroku.com/articles/rails-asset-pipeline

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module Iqvoc::SimilarTerms
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Heroku tweak
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = "1.0.0"
   end
