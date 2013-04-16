4313abb3def97979115903dc1cd3c24ec9362169
FND <fnd@localhost.localdomain>
2013-04-16 11:18:28 +0200

    moved localized routes into routes.rb
    empirically, it appears that load order is as follows:
    engine initializers, engine routes, core routes
    
    thus it seems fine to put it here - let's hope it stays that way

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+Iqvoc.localized_routes << lambda do |routing|
+  # XXX: adjective as resource
+  routing.get "similar" => "similar_terms#show"
+end
+
 Rails.application.routes.draw do
   # see configuration (initializer)
 end
