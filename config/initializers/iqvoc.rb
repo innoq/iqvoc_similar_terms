36815c7973a69c404d9cf3dbcfd8663b4ca0580c
FND <fnd@localhost.localdomain>
2013-04-16 12:33:09 +0200

    added Similar Terms to nav menu

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,1 +1,6 @@
 Iqvoc.config.register_setting("title", "iQvoc Similar Terms")
+
+Iqvoc.navigation_items.insert(-6, { # in front of search, users, configuration, help and about
+  :content => proc { link_to "Similar Terms", similar_path }, # TODO: i18n
+  :controller => "similar_terms"
+})
