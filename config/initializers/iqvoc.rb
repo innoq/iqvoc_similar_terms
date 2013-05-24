bb0489d3ea063f2c9a6faeb4072297fac6fecbdd
Robert Glaser <mail@robert-glaser.de>
2013-05-24 17:18:51 +0200

    fix navigation item grouping

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,9 +1,7 @@
 Iqvoc.config.register_setting("title", "iQvoc Similar Terms")
 
-ActiveSupport.on_load :navigation_services_group_created do
-  Iqvoc.navigation_items[-3][:items] << {
-    :text  => proc { t("txt.views.similar_terms.title") },
-    :href  => proc { similar_path },
-    :controller => "similar_terms"
-  }
-end
+Iqvoc::Navigation.add_grouped({
+  :text  => proc { t("txt.views.similar_terms.title") },
+  :href  => proc { similar_path },
+  :controller => "similar_terms"
+}, -3)
