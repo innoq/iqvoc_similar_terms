8e45fd5d99e3e5b4f0b0fa50e8b7a1648f0f26bc
Robert Glaser <mail@robert-glaser.de>
2013-11-22 12:57:43 +0100

    we don't test rails core functionality

--- test/functional/api_test.rb
+++ test/functional/api_test.rb
@@ -20,13 +20,6 @@ class SimilarTermsTest < ActionController::TestCase
   end
 
   test "routing" do
-    assert_raises ActionController::RoutingError do
-      get :show
-    end
-    assert_raises ActionController::RoutingError do
-      get :show, :format => "ttl"
-    end
-
     get :show, :lang => "en", :format => "ttl"
     assert_response 400
 
