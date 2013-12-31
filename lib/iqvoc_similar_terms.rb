368cd539fe469b0953c66b18ed5063a2fca8f7bc
Robert Glaser <mail@robert-glaser.de>
2013-12-31 13:24:49 +0100

    dry up app/engine check

--- lib/iqvoc_similar_terms.rb
+++ lib/iqvoc_similar_terms.rb
@@ -4,7 +4,19 @@ require 'iqvoc/similar_terms/version'
 
 module IqvocSimilarTerms
 
-  unless Iqvoc.const_defined?(:SimilarTerms) && Iqvoc::SimilarTerms.const_defined?(:Application)
+  def self.app?
+    Iqvoc.const_defined?(:SimilarTerms) && Iqvoc::SimilarTerms.const_defined?(:Application)
+  end
+
+  def self.root
+    if app?
+      Rails.root
+    else
+      Iqvoc::SimilarTerms::Engine.root
+    end
+  end
+
+  unless app?
     require File.join(File.dirname(__FILE__), '../config/engine')
   end
 
