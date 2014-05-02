04dacc6ebe45705ec485db3f92d28d610993533c
Robert Glaser <mail@robert-glaser.de>
2014-05-02 17:45:22 +0200

    adopt Iqvoc.routing_constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-Iqvoc.localized_routes << lambda do |routing|
-  # XXX: adjective as resource
-  routing.get "similar" => "similar_terms#show"
-  routing.post "similar" => "similar_terms#query"
-end
-
 Rails.application.routes.draw do
-  # see above
+  scope ":lang", :constraints => Iqvoc.routing_constraint do
+    get "similar" => "similar_terms#show"
+    post "similar" => "similar_terms#query"
+  end
 end
